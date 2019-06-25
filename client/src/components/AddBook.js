032bf3656d2fdfc2d5c94ac1fbfd7b46c5674ac4
Volodymyr Melnyk <melnykvova90@gmail.com>
2019-06-25 17:05:51 +0300

    add connect mutations to frontend

---
+++ client/src/components/AddBook.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {graphql, compose} from 'react-apollo';
+import {getAuthorsQuery, addBookMutation} from '../queries/queries';
+
+class AddBook extends Component {
+
+    state = {
+        name: '',
+        genre: '',
+        authorId: ''
+    }
+
+    displayAuthors = () => {
+        const { data }  = this.props;
+        if (data.loading) {
+            return <option disabled>Loading ...</option>
+        } else {
+            return data.authors.map(el => <option value={el.id} key={el.id}>{el.name}</option>)
+        }
+    }
+
+    inputChange = ({target}) => {
+        const {name, value} = target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        this.props.addBookMutation();
+    }
+
+    render() {
+    // console.log(this.props);
+        return (
+            <form id="add-book" onSubmit={this.onSubmit}>
+                <div className="field">
+                    <label>Book name:</label>
+                    <input type="text" name='name' onChange={this.inputChange}/>
+                </div>
+                <div className="field">
+                    <label>Genre:</label>
+                    <input type="text" name='genre' onChange={this.inputChange}/>
+                </div>
+                <div className="field">
+                    <label>Author:</label>
+                    <select name='authorId' onChange={this.inputChange}>
+                        <option>Select author</option>
+                        {this.displayAuthors()}
+                    </select>
+                </div>
+                <button>+</button>
+            </form>
+        );
+    }
+}
+
+export default compose(
+    graphql(getAuthorsQuery, {name: "data"}),
+    graphql(addBookMutation, {name: "addBookMutation"})
+)(AddBook);
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,15 +1,6 @@
 import React, {Component} from 'react';
-import {gql} from 'apollo-boost';
 import {graphql} from 'react-apollo';
-
-const getBooksQuery = gql `
- {
-     books {
-         name
-         id
-     }
- }
-`
+import {getBooksQuery} from '../queries/queries';
 
 class BookList extends Component {
 
@@ -26,7 +17,7 @@ class BookList extends Component {
     }
   }
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     return (
       <div>
         <ul className="book-list">
